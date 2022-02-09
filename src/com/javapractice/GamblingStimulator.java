471afafd38b332cf8f340244badb780086f5da8b
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-09 12:28:14 +0530

    Added:Gambler stake of 00 every day and bet  every game

--- src/com/javapractice/GamblingStimulator.java
+++ src/com/javapractice/GamblingStimulator.java
@@ -3,6 +3,9 @@ package com.javapractice;
 public class GamblingStimulator {
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to gambling simulator");	
+		System.out.println("Welcome to gambling simulator");
+		
+		int stake = 100;
+		int bet = 1 ;
 	}
 }
