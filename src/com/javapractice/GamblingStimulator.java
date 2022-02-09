92c7e981c793ca88ba3116094caf78dbaba69ca4
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-09 13:32:23 +0530

    Gambler make a bet of  so either win or lose

--- src/com/javapractice/GamblingStimulator.java
+++ src/com/javapractice/GamblingStimulator.java
@@ -2,10 +2,20 @@ package com.javapractice;
 
 public class GamblingStimulator {
 	public static void main(String[] args) {
+		int stake = 100;
+		int bet = 1 ;
 		
 		System.out.println("Welcome to gambling simulator");
 		
-		int stake = 100;
-		int bet = 1 ;
+		double Betting = Math.floor(Math.random()*100)%2;
+		if (Betting ==bet) {
+			stake++;
+			System.out.println("Gambler is winning $1");
+			System.out.println("stake" +stake);
+		}
+		else {
+			stake--;
+			System.out.println("Gambler is loosing");
+		}
 	}
 }
